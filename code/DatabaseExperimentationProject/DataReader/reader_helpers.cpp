d5c8f098c70d7c47d7a0a1e30e3b5721cb8313be
Patrick Kostjens <patrickkostjens@gmail.com>
2015-10-17 14:57:14 +0200

    Refactoring in preparation for more table types to be read

---
+++ code/DatabaseExperimentationProject/DataReader/reader_helpers.cpp
@@ -0,0 +1,28 @@
+#include "reader_helpers.h"
+#include "fstream"
+
+const tm ParseDate(const std::string& date_string) {
+	time_t raw_time;
+	time(&raw_time);
+	struct tm date;
+	localtime_s(&date, &raw_time);
+	date.tm_year = atoi(date_string.substr(0, 4).c_str());
+	date.tm_mon = atoi(date_string.substr(5, 2).c_str()) - 1;
+	date.tm_mday = atoi(date_string.substr(8, 2).c_str());
+
+	return date;
+}
+
+void ReadUntilSeparator(std::ifstream& in_file, char* out, const int len) {
+	std::string buf;
+	std::getline(in_file, buf, '|');
+
+	strcpy_s(out, len, buf.c_str());
+}
+
+const tm ReadDate(std::ifstream& in_file) {
+	std::string buf;
+	std::getline(in_file, buf, '|');
+
+	return ParseDate(buf);
+}
---
+++ code/DatabaseExperimentationProject/DataReader/reader_helpers.h
@@ -0,0 +1,8 @@
+#include "time.h"
+#include "string"
+
+const tm ParseDate(const std::string& date_string);
+
+void ReadUntilSeparator(std::ifstream& in_file, char* out, const int len);
+
+const tm ReadDate(std::ifstream& in_file);
