d5385bb0278243a6f8f6f6da5372d88dfc36bae9
Patrick Kostjens <patrickkostjens@gmail.com>
2015-10-27 12:40:11 +0100

    Added performance metric utility to print memory usage

---
+++ code/DatabaseExperimentationProject/Utils/performance_metrics.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "windows.h"
+#include "psapi.h"
+
+void PrintMemoryInfo()
+{
+	PROCESS_MEMORY_COUNTERS pmc;
+	GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc));
+	SIZE_T virtualMemUsedByMe = pmc.WorkingSetSize;
+
+	const float MBConversion = 1024 * 1024;
+	printf("Memory used: %6.2fMB\n", (float)pmc.WorkingSetSize / MBConversion);
+}
