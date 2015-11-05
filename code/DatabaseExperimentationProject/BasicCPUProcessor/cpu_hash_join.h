4f1c0665d5c82919f9e0a23610de3a13e8aa3365
Patrick Kostjens <patrickkostjens@gmail.com>
2015-11-05 14:18:30 +0100

    Improved CPU hash join by always creating hash table for smaller relationship

--- code/DatabaseExperimentationProject/BasicCPUProcessor/cpu_hash_join.h
+++ code/DatabaseExperimentationProject/BasicCPUProcessor/cpu_hash_join.h
@@ -13,46 +13,65 @@ private:
 		return (std::clock() - since) / (double)CLOCKS_PER_SEC * 1000;
 	}
 
-public:
-	CPUHashJoin(const std::vector<Left>& leftData, const std::vector<Right>& rightData){
-		_leftData = &leftData;
-		_rightData = &rightData;
-		return;
-	};
+	std::tuple<Left, Right> MakeTuple(const Left& left, const Right& right) {
+		return std::tuple<Left, Right>(left, right);
+	}
 
-	template <typename Comparable>
-	std::vector<std::tuple<Left, Right>>& Join(Comparable(*leftSelector)(const Left), Comparable(*rightSelector)(const Right)){
-		typedef std::tuple<Left, Right> JoinTuple;
+	std::tuple<Left, Right> MakeTuple(const Right& right, const Left& left) {
+		return std::tuple<Left, Right>(left, right);
+	}
 
+	template <typename Key, class Value>
+	void CreateHashTable(const std::vector<Value>* input, Key(*keySelector)(const Value), 
+		std::unordered_multimap<Key, const Value&>& hashTable) {
 		std::clock_t start = std::clock();
-		std::unordered_multimap<Comparable, const Left&> hashTable;
-
-		for (unsigned int i = 0; i < _leftData->size(); i++) {
-			const Left& element = _leftData->at(i);
-			auto pair = std::pair<Comparable, const Left&>(leftSelector(element), element);
+		for (unsigned int i = 0; i < input->size(); i++) {
+			const Value& element = input->at(i);
+			auto pair = std::pair<Key, const Value&>(keySelector(element), element);
 			hashTable.insert(pair);
 		}
 		std::cout << "Building hash table took " << GetElapsedTime(start) << "ms\n";
+	}
 
-		start = std::clock();
-		std::vector<JoinTuple>& result = *new std::vector<JoinTuple>();
+	template <typename Comparable, typename Hashed, typename Scanned>
+	std::vector<std::tuple<Left, Right>>& PerformJoin(std::unordered_multimap<Comparable, const Hashed&> hashTable,
+		const std::vector<Scanned> *otherInput, Comparable(*otherSelector)(const Scanned)) {
+		std::clock_t start = std::clock();
+		std::vector<std::tuple<Left, Right>>& result = *new std::vector<std::tuple<Left, Right>>();
 
-		for (unsigned int i = 0; i < _rightData->size(); i++) {
-			const Right& rightElement = _rightData->at(i);
-			Comparable key = rightSelector(rightElement);
+		for (unsigned int i = 0; i < otherInput->size(); i++) {
+			const Scanned& otherElement = otherInput->at(i);
+			Comparable key = otherSelector(otherElement);
 			auto& localResults = hashTable.find(key);
+
+			// Join with every hashed row with the same key
 			while (localResults != hashTable.end() && localResults->first == key) {
-				result.push_back(JoinTuple(localResults->second, rightElement));
+				result.push_back(MakeTuple(localResults->second, otherElement));
 				localResults++;
 			}
 		}
 		std::cout << "Joining using hash table took " << GetElapsedTime(start) << "ms\n";
-		
-
-		/*for (unsigned int i = 0; i < result.size(); i++) {
-			JoinTuple &resultElement = result.at(i);
-			std::cout << "Left: " << std::get<0>(resultElement) << "; Right: " << std::get<1>(resultElement) << "\n";
-		}*/
 		return result;
+	}
+
+public:
+	CPUHashJoin(const std::vector<Left>& leftData, const std::vector<Right>& rightData) {
+		_leftData = &leftData;
+		_rightData = &rightData;
+	};
+
+	template <typename Comparable>
+	std::vector<std::tuple<Left, Right>>& Join(Comparable(*leftSelector)(const Left), Comparable(*rightSelector)(const Right)) {
+		// Always create a hash table for the smaller relation
+		if (_leftData->size() <= _rightData->size()) {
+			std::unordered_multimap<Comparable, const Left&> hashTable;
+			CreateHashTable<Comparable, Left>(_leftData, leftSelector, hashTable);
+			return PerformJoin(hashTable, _rightData, rightSelector);
+		}
+		else {
+			std::unordered_multimap<Comparable, const Right&> hashTable;
+			CreateHashTable<Comparable, Right>(_rightData, rightSelector, hashTable);
+			return PerformJoin(hashTable, _leftData, leftSelector);
+		}
 	};
 };
