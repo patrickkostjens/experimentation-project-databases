40d2fdd8b223cee46c263e7bf45f3de152f36a53
Patrick Kostjens <patrickkostjens@gmail.com>
2015-11-07 13:35:13 +0100

    Reduced code duplication for GPU queries even further

--- code/DatabaseExperimentationProject/Main/gpu_queries.cpp
+++ code/DatabaseExperimentationProject/Main/gpu_queries.cpp
@@ -6,11 +6,11 @@
 #include "async_gpu_filter.h"
 #include "helpers.h"
 
-template<typename F>
-void RunGenericLineItemFilter(char* name, std::vector<LineItem>& orders, F& filterLambda) {
-	std::cout << "Running line items " << name << "\n";
+template<typename Filter, typename Item>
+void RunGenericFilter(char* filterName, char* typeName, std::vector<Item>& orders, Filter& filterLambda) {
+	std::cout << "Running " << typeName << " " << filterName << "\n";
 	std::clock_t start = std::clock();
-	std::vector<LineItem>& results = filterLambda(orders);
+	std::vector<Item>& results = filterLambda(orders);
 	size_t resultCount = results.size();
 
 	double duration = GetElapsedTime(start);
@@ -21,31 +21,17 @@ void RunGenericLineItemFilter(char* name, std::vector<LineItem>& orders, F& filt
 }
 
 void RunGPUFilter(std::vector<LineItem>& items) {
-	RunGenericLineItemFilter("standard GPU processor", items, gpu_filter<LineItem>);
+	RunGenericFilter("standard GPU processor", "line item", items, gpu_filter<LineItem>);
 	// First run takes significantly longer, so run twice
-	RunGenericLineItemFilter("standard GPU processor", items, gpu_filter<LineItem>);
-	RunGenericLineItemFilter("Unified Memory GPU processor", items, um_gpu_filter<LineItem>);
-	RunGenericLineItemFilter("async GPU processor", items, gpu_filter_async<LineItem>);
-}
-
-template<typename F>
-void RunGenericOrderFilter(char* name, std::vector<Order>& orders, F& filterLambda) {
-	std::cout << "Running orders " << name << "\n";
-	std::clock_t start = std::clock();
-	std::vector<Order>& results = filterLambda(orders);
-	size_t resultCount = results.size();
-
-	double duration = GetElapsedTime(start);
-	std::cout << "GPU result count: " << resultCount << "\n";
-	std::cout << "GPU processing took " << duration << "ms\n\n";
-
-	delete &results;
+	RunGenericFilter("standard GPU processor", "line item", items, gpu_filter<LineItem>);
+	RunGenericFilter("Unified Memory GPU processor", "line item", items, um_gpu_filter<LineItem>);
+	RunGenericFilter("async GPU processor", "line item", items, gpu_filter_async<LineItem>);
 }
 
 void RunGPUFilter(std::vector<Order>& orders) {
-	RunGenericOrderFilter("standard GPU processor", orders, gpu_filter<Order>);
+	RunGenericFilter("standard GPU processor", "orders", orders, gpu_filter<Order>);
 	// First run takes significantly longer, so run twice
-	RunGenericOrderFilter("standard GPU processor", orders, gpu_filter<Order>);
-	RunGenericOrderFilter("Unfied Memory GPU processor", orders, um_gpu_filter<Order>);
-	RunGenericOrderFilter("async GPU processor", orders, gpu_filter_async<Order>);
+	RunGenericFilter("standard GPU processor", "orders", orders, gpu_filter<Order>);
+	RunGenericFilter("Unfied Memory GPU processor", "orders", orders, um_gpu_filter<Order>);
+	RunGenericFilter("async GPU processor", "orders", orders, gpu_filter_async<Order>);
 }
